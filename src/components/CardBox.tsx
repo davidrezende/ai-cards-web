97b3d401695dd2a755bf415dc503be019ac70c4d
enlaitc <tccdevop@gmail.com>
2023-06-28 23:20:28 -0300

    mudanças no card inventory e na visualização da carta

--- src/components/CardBox.tsx
+++ src/components/CardBox.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cardDefault from '../assets/magonegro.png'
-import Image from '../shared/types/ImageVO';
-import Attribute from '../shared/types/AttributeVO';
 import Card from '../shared/types/CardVO';
 
 type ListProps = {
@@ -13,17 +11,10 @@ export const CardBox: React.FC<ListProps> = ({ card, onShowDialog }) => {
 
   return (
     <>
-      <div onClick={() => onShowDialog(card)} className=" flex items-center justify-center hover:scale-125">
-        <div className="p-4 rounded-lg border-solid border-2 bg-base-200 border-gray-700 card shadow-xl w-full h-full ">
+      <div onClick={() => onShowDialog(card)} className="flex items-center justify-center hover:scale-125 pb-3">
+        <div className="p-3 rounded-box border-solid border-2 bg-base-200 border-gray-700 card shadow-xl w-full h-full">
           <p className='font-bold truncate text-center pb-2'>{card.name}</p>
-          <figure><img className="rounded-lg" src={!!!card || !!!card.image || !!!card.image.image ? cardDefault : 'data:image/jpeg;base64,' + card.image.image} alt={card.name} /></figure>
-          {/* <p>{card.description}</p>
-          <ul className="list-none">
-            <li>FORÇA: {card.attributes.FORCA}</li>
-            <li>DESTREZA: {card.attributes.DESTREZA}</li>
-            <li>VITALIDADE: {card.attributes.VITALIDADE}</li>
-            <li>INTELIGENCIA: {card.attributes.INTELIGENCIA}</li>
-          </ul> */}
+          <img className="rounded-box" src={!!!card || !!!card.image || !!!card.image.image ? cardDefault : 'data:image/jpeg;base64,' + card.image.image} alt={card.name} />
         </div>
       </div>
     </>
--- src/components/MenuProfile.tsx
+++ src/components/MenuProfile.tsx
@@ -5,7 +5,7 @@ import cardimg from './../assets/magonegro.png';
 export const MenuProfile: React.FC<any> = (props) => {
 
   return (
-    <Card className="flex-shrink-0 w-full items-center shadow-2xl bg-base-100 bg-opacity-80">
+    <Card className="w-full sm:w-1/3 lg:w-full items-center shadow-2xl bg-base-100 bg-opacity-80">
       <Card.Body className='flex  flex-col lg:flex-row'>
       <div className="card shadow-xl  ">
       <figure><img src="https://www.scotsman.com/webimg/b25lY21zOmRmYmExYzE4LTZhY2ItNDBkZS1iMTU1LWY4YTVlZWNmYTdkYzowOTcxZDZlOC00MDc1LTQzYzItOWEyOC00YjNlNzFiY2Y1YzI=.jpg?width=1200&enable=upscale" alt="Shoes" /></figure>
