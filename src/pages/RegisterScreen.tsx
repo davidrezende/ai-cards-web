ef241b2eb27383f75a953c189951141fb769273e
Enlai tc <tccdevop@gmail.com>
2024-02-27 22:51:29 -0300

    iniciando condicoes de alerta

--- src/pages/RegisterScreen.tsx
+++ src/pages/RegisterScreen.tsx
@@ -3,6 +3,7 @@ import useSignIn from "react-auth-kit/hooks/useSignIn"
 import { useNavigate } from 'react-router-dom'
 import { UserService } from "../services/ServiceUser"
 import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated'
+import Alerts from "../components/Alerts"
 
 export const RegisterScreen: React.FC<any> = () => {
 
@@ -12,6 +13,8 @@ export const RegisterScreen: React.FC<any> = () => {
   const [name, setName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [visible, setVisible] = useState(false)
+  const [alert, setAlert] = useState("")
 
   useEffect(() => {
     console.log('verificando se usuario logado')
@@ -32,32 +35,39 @@ export const RegisterScreen: React.FC<any> = () => {
         "password": password
       }
     ).then(() => {
-      alert("Cadastrado com Sucesso!");
-      navigate("/login")
-    }).catch ((err) => {
+      alertContext("success")
+    }).catch((err) => {
+      alertContext("error")
       console.log(err)
     })
   }
 
   // nao reload no form
-  var form = document.getElementById("myForm");
-  function submitForm(event: any) {
-    event.preventDefault();
+  const form = document.getElementById('myForm')
+  form?.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log()
+  })
+
+  function alertContext(type: string) {
+    setVisible(true)
+    setAlert(type)
   }
 
-  form?.addEventListener('submit', submitForm);
-
   return (
+
     <div className="w-1/4 justify-center px-6 py-12 lg:px-8 bg-white absolute bg-opacity-95">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Cadastro Ai-Cards
         </h2>
       </div>
-
+      {visible &&
+        (Alerts(alert))
+      }
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form id={"myForm"} className="space-y-6">
-        <div>
+        <form id={"myForm"} className="space-y-6" onSubmit={onSubmit}>
+          <div>
             <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
               Nome
             </label>
@@ -67,7 +77,6 @@ export const RegisterScreen: React.FC<any> = () => {
                 name="name"
                 type="name"
                 autoComplete="name"
-                onChange={e => setName(e.target.value)}
                 required
                 className="bg-white indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -84,7 +93,6 @@ export const RegisterScreen: React.FC<any> = () => {
                 name="email"
                 type="email"
                 autoComplete="email"
-                onChange={e => setEmail(e.target.value)}
                 required
                 className="bg-white indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -102,7 +110,6 @@ export const RegisterScreen: React.FC<any> = () => {
                 id="password"
                 name="password"
                 type="password"
-                onChange={e => setPassword(e.target.value)}
                 autoComplete="current-password"
                 required
                 className="bg-white indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -112,7 +119,7 @@ export const RegisterScreen: React.FC<any> = () => {
 
           <div>
             <button
-              onClick={onSubmit}
+              type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Registrar
@@ -123,7 +130,7 @@ export const RegisterScreen: React.FC<any> = () => {
         <p className="mt-10 text-center text-sm text-gray-500">
           Já tem cadastro?{' '}
           <a onClick={() => navigate("/login")} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer">
-            Sign-In  
+            Sign-In
           </a>
         </p>
       </div>
