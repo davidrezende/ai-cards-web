0fb2f5d3c16108308444fc60c355cc994420eb02
David Rezende <61335298+davidrezende@users.noreply.github.com>
2024-03-05 14:38:16 -0300

    Revert "Revert "criacao tela de registro""

--- src/services/ServiceUser.ts
+++ src/services/ServiceUser.ts
@@ -1,8 +1,11 @@
 import { UserAPI } from "../providers/ClientIntegradorAPI"
 import IRequestLoginUser from "../shared/types/RequestLoginUser"
+import IRequestRegisterUser from "../shared/types/RequestRegisterUser"
 
 const userLogin = (requestLogin: IRequestLoginUser) => UserAPI.post('/login', requestLogin)
+const userRegister = (requestRegister: IRequestRegisterUser) => UserAPI.post('/register', requestRegister)
 
 export const UserService = {
-    userLogin
+    userLogin,
+    userRegister
 }
