e8954ff33a9d9d11802e4abcfd94e8e4880cf404
David <davidpenalives@gmail.com>
2024-03-19 15:05:54 -0300

    Adjust layout

--- src/pages/InventoryScreen.tsx
+++ src/pages/InventoryScreen.tsx
@@ -60,7 +60,9 @@ const Dialog: React.FC<DialogProps> = ({ isOpen, onClose, cardPopup }) => {
             {cardPopup?.status == "CREATED" ?
 
                 <div className="h-full md:w-11/12 md:h-5/6 lg:w-7/12 lg:h-5/6 bg-base-300 rounded-lg shadow overflow-auto">
+                    
                     <div className='flex flex-col justify-center items-center p-6 '>
+                        
                         <>
                             <div className={`${classRarity} ${getCardClass(cardPopup?.rarity)} z-50 rounded-box`}>
                                 <div className=' bg-transparent flex lg:flex-col justify-center items-center h-full w-full p-5'>
@@ -84,36 +86,19 @@ const Dialog: React.FC<DialogProps> = ({ isOpen, onClose, cardPopup }) => {
                                         </ul>
                                     </div>
                                 </div>
+                                
 
-                            </div>
+                            </div><br />
+                            
                             <br />
                             <ul className="grid grid-rows-2 grid-flow-col w-full gap-x-2 gap-y-1 text-sm text-center">
                                 <li><b>COLEÇÃO: {cardPopup?.collectionSeries}</b></li>
                             </ul>
-                            <div className='lg:w-4/5 py-5'>
-                                <p className="max-w-max">{cardPopup?.description}</p>
-                            </div>
-                        </>
-
-
-                        <div className="w-full flex justify-center lg:w-4/5 ">
-                            <button
-                                onClick={onClose}
-                                className="btn bg- w-3/5"
-                            >
-                                <svg
-                                    viewBox="0 0 1024 1024"
-                                    fill="currentColor"
-                                    height="1em"
-                                    width="1em"
-                                >
-                                    <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" />
-                                </svg>
-                                Fechar
-                            </button>
+                            <div className="w-full flex justify-center lg:w-2/5 ">
+                            
                             <button
                                 onClick={() => navigate("/share/" + cardPopup.cardHash)}
-                                className="ml-4 btn bg-primary w-2/5"
+                                className="ml-4 btn bg-primary w-5/5"
                             >
                                 <svg
                                     viewBox="0 0 16 16"
@@ -128,6 +113,25 @@ const Dialog: React.FC<DialogProps> = ({ isOpen, onClose, cardPopup }) => {
                                 Compartilhar
                             </button>
                         </div>
+                            <div className='lg:w-4/5 py-5'>
+                                <p className="max-w-max">{cardPopup?.description}</p>
+                            </div>
+                        </><br /><br />
+                        <button
+                                onClick={onClose}
+                                className="btn bg- w-2/5"
+                            >
+                                <svg
+                                    viewBox="0 0 1024 1024"
+                                    fill="currentColor"
+                                    height="1em"
+                                    width="1em"
+                                >
+                                    <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" />
+                                </svg>
+                                Fechar
+                            </button>
+                            <br /><br />
                     </div>
                 </div>
                 :
@@ -183,10 +187,10 @@ export const InventoryScreen: React.FC<any> = () => {
     return (
         <>
             <Dialog isOpen={isDialogOpen} onClose={handleCloseDialog} cardPopup={cardPopup} />
-            <div className='h-screen lg:h-screen max-sm:h-lvh lg:flex lg:flex-col lg:overflow-auto'>
+            <div className={`${!!cards ? cards.length <= 0 ? 'h-screen' : '' : 'w-screen'} lg:h-screen max-sm:h-lvh lg:flex lg:flex-col lg:overflow-auto`}>
                 <NavbarApp />
                 <div className="flex flex-col lg:flex-row p-5 h-full">
-                    <div className="p-5 lg:w-1/6 md:w-full flex h-full card bg-base-300 rounded-box justify-center items-center">
+                    <div className="p-5 lg:w-2/6 md:w-full flex h-full card bg-base-300 rounded-box justify-center items-center">
                         {userData != undefined ? <MenuProfile userData={userData} /> : ''}
                     </div>
                     <div className="divider lg:divider-horizontal">
@@ -199,6 +203,7 @@ export const InventoryScreen: React.FC<any> = () => {
                         </div>
                         <div className="grid grid-cols-2 gap-5 md:gap-3 lg:gap-6 lg:grid-cols-6 md:grid-cols-4 sm:p-5 max-sm:h-screen overflow-auto">
                             {
+                                
                                 !!cards ? cards.length > 0 ? cards.map((card) => (
                                     <CardBox
                                         card={card}
